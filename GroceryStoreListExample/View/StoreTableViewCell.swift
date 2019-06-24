4e11d0f45020b57d66de8fb07a1246485e4619c0
Michael Miles <michael.gage.miles@gmail.com>
2019-06-24 10:22:47 -0700

    Added TableViewCell for stores and linked it up to storyboard.

---
+++ GroceryStoreListExample/View/StoreTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  StoreTableViewCell.swift
+//  GroceryStoreListExample
+//
+//  Created by Michael Miles on 6/24/19.
+//  Copyright © 2019 Michael Miles. All rights reserved.
+//
+
+import UIKit
+
+class StoreTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var storeImageView: UIImageView!
+    @IBOutlet weak var storeNameLabel: UILabel!
+    @IBOutlet weak var storePriceLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
